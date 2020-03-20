4ddb3ec048b58f9e5ecc4e11fb6283d75697ae30
Rafael <martin.cruz@thincode.com>
2020-03-20 13:06:25 -0600

    edit and delete actions

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -19,8 +19,23 @@ class PlaysController < ApplicationController
     end
   end
 
-  def show
-      
+  def show 
+  end
+
+  def edit
+  end
+
+  def update
+    if @play.update(play_params)
+      redirect_to play_path(@play)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @play.destroy
+    redirect_to root_path
   end
 
   private
