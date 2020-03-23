882c3cb34641ac21eff2657b9770b449e52042ad
Rafael <martin.cruz@thincode.com>
2020-03-23 11:21:09 -0600

    render images on index page

--- app/models/play.rb
+++ app/models/play.rb
@@ -1,7 +1,8 @@
-class Play < ActiveRecord::Base
+class Play < ApplicationRecord
   belongs_to :user
   belongs_to :category
 
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+  has_attached_file :play_img, styles: { :play_index => "250x350>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :play_img, content_type: /\Aimage\/.*\z/
+
 end
